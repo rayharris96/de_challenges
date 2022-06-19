314f88d0885f6c018104dbcfa46cf3dac192c8eb
Raymond <harris96raymond@gmail.com>
2022-06-19 21:27:55 +0800

    Added docker instructions and table DDL creation

---
+++ section_2/pg_init/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE sales (
+    id BIGINT NOT NULL,
+    salesperson_id INT NOT NULL,
+    customer_id BIGINT NOT NULL,
+    serial_number VARCHAR(30) NOT NULL,
+    price FLOAT NOT NULL
+);
+
+CREATE TABLE cars (
+    serial_number BIGINT NOT NULL,
+    manufacturer TEXT NOT NULL,
+    model_name TEXT NOT NULL,
+    weight FLOAT NULL
+);
+
+CREATE TABLE customers (
+    customer_id BIGINT NOT NULL,
+    name TEXT NOT NULL,
+    phone VARCHAR(8) NULL
+);
+
+CREATE TABLE salesperson (
+    salesperson_id INT NOT NULL,
+    name TEXT NOT NULL,
+    phone VARCHAR(8) NULL
+);
