acebd943514123aa0bec7f982926a00c8ef77077
Raymond <harris96raymond@gmail.com>
2022-06-19 16:23:30 +0800

    Added initdb script to load in raw data

---
+++ section_1/pgsetup/load_tables.sql
@@ -0,0 +1,15 @@
+-- Create table and load the table into Postgres database
+-- Table type is varchar and null to account for dirty datasets
+CREATE TABLE dataset1(
+    name VARCHAR NULL,
+    price VARCHAR NULL
+)
+
+CREATE TABLE dataset2(
+    name VARCHAR NULL,
+    price VARCHAR NULL
+)
+
+-- Copy dataset from raw csv
+COPY dataset1 FROM '../raw_tables/dataset1.csv' WITH (FORMAT csv);
+COPY dataset2 FROM '../raw_tables/dataset2.csv' WITH (FORMAT csv);
